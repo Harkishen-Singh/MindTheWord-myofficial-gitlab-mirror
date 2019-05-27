b7ecdf9e5fa96c0ed65d7f658b8a97c743804d53
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-27 17:25:49 +0530

    fixed id bugs, ci tests for blacklist and pattern operations

--- test/webdriver.js
+++ test/webdriver.js
@@ -1,7 +1,9 @@
 const webdriver = require('selenium-webdriver'),
   chrome = require('selenium-webdriver/chrome'),
   actionSequence = webdriver.actionSequence,
-  bootstrapTourTests = require('./components/bootstrapTour_variables'),
+  bootstrapTourVars = require('./components/bootstrapTour_variables'),
+  patternOperationsVars = require('./components/patternsCreation_variables'),
+  blacklisttestVars = require('./components/blacklistTest_variables'),
   Key = webdriver.Key,
   By = webdriver.By;
 
@@ -16,7 +18,7 @@ chromeOptions.addArguments('--disable-dev-shm-usage');
 chromeOptions.addArguments('--disable-gpu');
 chromeOptions.addArguments('--load-extension=dist/');
 
-function resetHomePage() {
+function reloadToHomePage() {
   describe('reset to home screen', function() {
     it('set screen as home page', (done) => {
       driver.get('chrome-extension://jaodmdnaglgheeibgdcgdbhljooejiha/views/options.html').then(() => {
@@ -25,6 +27,12 @@ function resetHomePage() {
     });
   });
 }
+
+function scrollToElementByxPath(text) {
+  element = driver.findElement(By.xpath(text));
+  driver.executeScript('arguments[0].scrollIntoView()', element);
+  driver.sleep(300);
+};
 
 describe('Executing tests in chrome environments', function() {
   // global test timeout
@@ -52,14 +60,14 @@ describe('Executing tests in chrome environments', function() {
 
   describe('Bootstrap tour test', function() {
     it('welcome screen', (done) => {
-      driver.findElement(By.xpath(bootstrapTourTests.nextWelcome)).click().then(e => {
+      driver.findElement(By.xpath(bootstrapTourVars.nextWelcome)).click().then(e => {
         done();
       });
     });
 
     it('translation tab', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextTranslation)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextTranslation)).click().then(e => {
           done();
         });
       }, 500);
@@ -67,7 +75,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('translator keys', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextTranslatorkeys)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextTranslatorkeys)).click().then(e => {
           done();
         });
       }, 500);
@@ -75,7 +83,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('learning patterns', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextCreateLP)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextCreateLP)).click().then(e => {
           done();
         });
       }, 500);
@@ -83,7 +91,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('translation patterns', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextTP)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextTP)).click().then(e => {
           done();
         });
       }, 500);
@@ -91,7 +99,7 @@ describe('Executing tests in chrome environments', function() {
     
     it('blacklist tab', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextBlacklistTab)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextBlacklistTab)).click().then(e => {
           done();
         });
       }, 500);
@@ -99,7 +107,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('blacklist websites', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextBW)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextBW)).click().then(e => {
           done();
         });
       }, 500);
@@ -108,7 +116,7 @@ describe('Executing tests in chrome environments', function() {
     
     it('blacklist words', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextBWords)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextBWords)).click().then(e => {
           done();
         });
       }, 500);
@@ -116,7 +124,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('learning tab', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextLearningTab)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextLearningTab)).click().then(e => {
           done();
         });
       }, 500);
@@ -124,7 +132,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('saved translation', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextSavedTranslationDiv)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextSavedTranslationDiv)).click().then(e => {
           done();
         });
       }, 500);
@@ -132,7 +140,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('learnt words', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextLearntWordsDiv)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextLearntWordsDiv)).click().then(e => {
           done();
         });
       }, 500);
@@ -140,7 +148,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('advance settings', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextAdv)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextAdv)).click().then(e => {
           done();
         });
       }, 500);
@@ -148,7 +156,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('backup', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextBackUp)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextBackUp)).click().then(e => {
           done();
         });
       }, 500);
@@ -156,7 +164,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('quiz', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextQuiz)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextQuiz)).click().then(e => {
           done();
         });
       }, 500);
@@ -164,7 +172,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('statistics', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextStats)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextStats)).click().then(e => {
           done();
         });
       }, 500);
@@ -172,7 +180,7 @@ describe('Executing tests in chrome environments', function() {
 
     it('contribute', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextContribute)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextContribute)).click().then(e => {
           done();
         });
       }, 500);
@@ -180,14 +188,198 @@ describe('Executing tests in chrome environments', function() {
 
     it('finish tour', (done) => {
       setTimeout(() => {
-        driver.findElement(By.xpath(bootstrapTourTests.nextTourFinish)).click().then(e => {
+        driver.findElement(By.xpath(bootstrapTourVars.nextTourFinish)).click().then(e => {
           done();
         });
       }, 500);
     });
 
-    resetHomePage();
+    reloadToHomePage();
   });
 
+  describe('Pattern Operations', function() {
+
+    it('assign random api keys', (done) => {
+      driver.findElement(By.xpath(patternOperationsVars.yandexInputField)).then(field1 => {
+        field1.sendKeys('randomYandexKey1');
+        driver.findElement(By.xpath(patternOperationsVars.azureInputField)).then(field2 => {
+          field2.sendKeys('randomAzureKey1');
+          driver.findElement(By.xpath(patternOperationsVars.googleInputField)).then(field3 => {
+            field3.sendKeys('randomGoogleKey1');
+            // operations
+            driver.manage().window().maximize();
+            done();
+          });
+        });
+      });
+    });
+
+    describe('creating patterns', function() {
+      it('Yandex', (done) => {
+        driver.findElement(By.xpath(patternOperationsVars.patternTranslator)).sendKeys('Yandex').then(() => {
+          driver.findElement(By.xpath(patternOperationsVars.patternPercent)).sendKeys('70').then(() => {
+            driver.findElement(By.xpath(patternOperationsVars.patternSrcLang)).sendKeys('English').then(() => {
+              driver.findElement(By.xpath(patternOperationsVars.patternTargetLang)).sendKeys('Hindi').then(() => {
+                setTimeout(() => {
+                  let element = driver.findElement(By.xpath(patternOperationsVars.patternCreationDiv));
+                  driver.executeScript('arguments[0].scrollIntoView()', element).then(() => {
+                    driver.findElement(By.xpath(patternOperationsVars.patternCreateButton)).click().then(() => {
+                      done();
+                    });
+                  });
+                }, 500);
+              });
+            });
+          });
+        });
+      });
+      
+      it('Azure', (done) => {
+        driver.findElement(By.xpath(patternOperationsVars.patternTranslator)).sendKeys('Azure').then(() => {
+          driver.findElement(By.xpath(patternOperationsVars.patternPercent)).sendKeys('60').then(() => {
+            driver.findElement(By.xpath(patternOperationsVars.patternSrcLang)).sendKeys('English').then(() => {
+              driver.findElement(By.xpath(patternOperationsVars.patternTargetLang)).sendKeys('Hindi').then(() => {
+                setTimeout(() => {
+                  let element = driver.findElement(By.xpath(patternOperationsVars.patternCreationDiv));
+                  driver.executeScript('arguments[0].scrollIntoView()', element).then(() => {
+                    driver.findElement(By.xpath(patternOperationsVars.patternCreateButton)).click().then(() => {
+                      done();
+                    });
+                  });
+                }, 500);
+              });
+            });
+          });
+        });
+      });
+
+      it('Google', (done) => {
+        driver.findElement(By.xpath(patternOperationsVars.patternTranslator)).sendKeys('Google').then(() => {
+          driver.findElement(By.xpath(patternOperationsVars.patternPercent)).sendKeys('50').then(() => {
+            driver.findElement(By.xpath(patternOperationsVars.patternSrcLang)).sendKeys('English').then(() => {
+              driver.findElement(By.xpath(patternOperationsVars.patternTargetLang)).sendKeys('Hindi').then(() => {
+                setTimeout(() => {
+                  let element = driver.findElement(By.xpath(patternOperationsVars.patternCreationDiv));
+                  driver.executeScript('arguments[0].scrollIntoView()', element).then(() => {
+                    driver.findElement(By.xpath(patternOperationsVars.patternCreateButton)).click().then(() => {
+                      done();
+                    });
+                  });
+                }, 500);
+              });
+            });
+          });
+        });
+      });
+    });
+
+    describe('check pattern', function() {
+      // reload the page to check angular modules and local chrome
+      it('check translation patterns', (done) => {
+        reloadToHomePage();
+        driver.findElement(By.xpath(patternOperationsVars.createdYandex)).then(() => {
+          // yandex pattern found
+          driver.findElement(By.xpath(patternOperationsVars.createdAzure)).then(() => {
+            // azure pattern found
+            driver.findElement(By.xpath(patternOperationsVars.createdGoogle)).then(() => {
+              // google pattern found
+              done();
+            });
+          });
+        });
+      });
+
+      it('toggling patterns', (done) => {
+        // the active symbol exists on the last created pattern
+        driver.executeScript('window.scrollTo(0,0)').then(() => {
+          // scrolled to page top
+          driver.findElement(By.xpath(patternOperationsVars.activeSymbolLastPattern)).then(() => {
+            // last pattern active
+            driver.findElement(By.xpath(patternOperationsVars.createdYandex)).click().then(() => {
+              // activated the first pattern
+              driver.findElement(By.xpath(patternOperationsVars.activeSymbolFirstPattern)).then(() => {
+                // checked the first pattern to be active
+                done();
+              });
+            });
+          });
+        });
+      });
   
+      it('deleting patterns', (done) => {
+        driver.findElement(By.xpath(patternOperationsVars.deleteThirdPattern)).click().then(() => {
+          // deleted third pattern
+          driver.findElement(By.xpath(patternOperationsVars.deleteSecondPattern)).click().then(() => {
+            // deleted second pattern
+            driver.findElement(By.xpath(patternOperationsVars.deleteFirstPattern)).click().then((res) => {
+              // deleted first pattern
+              driver.findElement(By.xpath(patternOperationsVars.createdYandex)).then(() => {
+                // failed to delete element
+                throw new Error('unable to delete translation patterns');
+              },(err) => {
+                // element was deleted
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+
+  });
+
+  describe('Blacklist functionality', function() {
+    it('open blacklist page', (done) => {
+      driver.findElement(By.xpath('//*[@id="blacklisting-nav"]')).click().then(() => {
+        done();
+      });
+    });
+
+    it('whitelisting website', (done) => {
+      driver.findElement(By.xpath(blacklisttestVars.firstBlacklistWebsiteElement)).getText().then((text) => {
+        driver.findElement(By.xpath(blacklisttestVars.firstBlacklistWebsiteButton)).click().then(() => {
+          // removed from blacklist
+          driver.findElement(By.xpath(blacklisttestVars.firstBlacklistWebsiteElement)).getText().then((text2) => {
+            if (text !== text2) {
+              done();
+            } else {
+              throw new Error('Website could not be removed from the blacklist');
+            }
+          });
+        });
+      });
+    });
+
+    it('blacklisting website', (done) => {
+      driver.findElement(By.xpath(blacklisttestVars.bl_websitesInput)).sendKeys('stackoverflow.com').then(() => {
+        driver.findElement(By.xpath(blacklisttestVars.add_bl_websites)).click().then(() => {
+          done();
+        });
+      });
+    });
+
+    it('whitelisting words', (done) => {
+      driver.findElement(By.xpath(blacklisttestVars.firstBlacklistWordElement)).getText().then((text) => {
+        driver.findElement(By.xpath(blacklisttestVars.firstBlacklistWordButton)).click().then(() => {
+          // removed from blacklist
+          driver.findElement(By.xpath(blacklisttestVars.firstBlacklistWordElement)).getText().then((text2) => {
+            if (text !== text2) {
+              done();
+            } else {
+              throw new Error('word could not be removed from the blacklist');
+            }
+          });
+        });
+      });
+    });
+  
+    it('blacklisting words', (done) => {
+      driver.findElement(By.xpath(blacklisttestVars.bl_wordsInput)).sendKeys('this').then(() => {
+        driver.findElement(By.xpath(blacklisttestVars.add_bl_words)).click().then(() => {
+          done();
+        });
+      });
+    });
+
+  });
 }); 
