d04f92f210dda6f6314f773f44aab40ae8cbe99e
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-30 18:33:46 +0530

    test6

--- test/webdriver.js
+++ test/webdriver.js
@@ -14,7 +14,7 @@ const webdriver = require('selenium-webdriver'),
   describe, it;
 } require('selenium-webdriver/testing');
 
-// require('chromedriver');
+require('chromedriver');
 let chromeOptions = new chrome.Options(),
   driver,
   yandexTranslationKey = process.env.yandex_key;
@@ -25,7 +25,7 @@ if (yandexTranslationKey === undefined) { // key absent
 }
 
 // adding browser options
-chromeOptions.setChromeBinaryPath(chromium.path);
+chromeOptions.setChromeBinaryPath('/usr/bin/chromium-browser');
 chromeOptions.addArguments('--no-sandbox');
 chromeOptions.addArguments('--disable-dev-shm-usage');
 chromeOptions.addArguments('--disable-gpu');
