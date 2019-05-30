d57ed5bcb9af98ec59bdc9d720ae817d47d56139
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-31 03:05:12 +0530

    this jspm 2

--- config.js
+++ config.js
@@ -15,46 +15,30 @@ System.config({
 
   map: {
     "angular": "github:angular/bower-angular@1.5.5",
-    "babel": "npm:babel-core@6.26.3",
-    "babel-runtime": "npm:babel-runtime@6.26.0",
     "bootstrap": "github:twbs/bootstrap@3.3.6",
     "core-js": "npm:core-js@1.2.6",
     "jquery": "npm:jquery@2.2.3",
     "lodash": "npm:lodash@4.14.0",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.5.0"
-    },
-    "github:jspm/nodelibs-buffer@0.1.1": {
-      "buffer": "npm:buffer@5.2.1"
+      "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.10"
+      "process": "npm:process@0.11.3"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "github:jspm/nodelibs-vm@0.1.0": {
-      "vm-browserify": "npm:vm-browserify@0.0.4"
-    },
     "github:twbs/bootstrap@3.3.6": {
       "jquery": "npm:jquery@2.2.3"
     },
-    "npm:assert@1.5.0": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
-      "object-assign": "npm:object-assign@4.1.1",
-      "process": "github:jspm/nodelibs-process@0.1.2",
+    "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:buffer@5.2.1": {
-      "base64-js": "npm:base64-js@1.3.0",
-      "ieee754": "npm:ieee754@1.1.13"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
@@ -68,17 +52,12 @@ System.config({
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:process@0.11.10": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "vm": "github:jspm/nodelibs-vm@0.1.0"
+    "npm:process@0.11.3": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:vm-browserify@0.0.4": {
-      "indexof": "npm:indexof@0.0.1"
     }
   }
 });
