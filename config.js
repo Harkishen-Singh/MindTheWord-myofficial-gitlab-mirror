6396a032750c5df54227e25ec8924c802999f184
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-31 02:01:28 +0530

    attempt

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ System.config({
     "angular": "github:angular/bower-angular@1.5.5",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
-    "bootstrap": "npm:bootstrap@4.3.1",
+    "bootstrap": "github:twbs/bootstrap@3.3.6",
     "core-js": "npm:core-js@1.2.6",
-    "jquery": "npm:jquery@2.2.4",
+    "jquery": "npm:jquery@2.2.3",
     "lodash": "npm:lodash@4.14.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -33,15 +33,14 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
+    "github:twbs/bootstrap@3.3.6": {
+      "jquery": "npm:jquery@2.2.3"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:bootstrap@4.3.1": {
-      "jquery": "npm:jquery@2.2.4",
-      "tether": "github:HubSpot/tether@1.4.4"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
