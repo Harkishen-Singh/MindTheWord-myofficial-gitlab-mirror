a669b1e7749af2861735ea368b5215b4cd4cffdd
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-31 00:59:40 +0530

    changing deps

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ System.config({
 
   map: {
     "angular": "github:angular/bower-angular@1.5.5",
-    "babel": "npm:babel-core@5.8.38",
-    "babel-runtime": "npm:babel-runtime@5.8.38",
-    "bootstrap": "github:twbs/bootstrap@3.3.6",
+    "babel": "npm:babel-core@^6.0.0",
+    "babel-runtime": "npm:babel-runtime@^6.0.0,
+    "bootstrap": "npm:bootstrap@4.3.1",
     "core-js": "npm:core-js@1.2.6",
-    "jquery": "npm:jquery@2.2.3",
+    "jquery": "npm:jquery@2.2.4",
     "lodash": "npm:lodash@4.14.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -33,14 +33,15 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "github:twbs/bootstrap@3.3.6": {
-      "jquery": "npm:jquery@2.2.3"
-    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:bootstrap@4.3.1": {
+      "jquery": "npm:jquery@2.2.4",
+      "tether": "github:HubSpot/tether@1.4.4"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
